d5eba27cbfe388b24b8f7c6d026ab6e7c045730b
Muhammad Naveed Khan <naveed@Apples-MacBook-Pro.local>
2023-09-24 22:23:25 +0500

    List post on the main post page.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,17 +1,44 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
 export default function Page() {
 
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        async function fetchDataAsync() {
+            const result = await fetchData();
+            setData(result);
+        }
+        fetchDataAsync();
+    }, []);
+
     return (
-    
+
         <>
-        <h1>Posts Page!</h1>
-        <h2>
-            <ul>
-                <li>Post 1</li>
-                <li>Post 2</li>
-                <li>Post 3</li>
-            </ul>
-        </h2>
+            <h1>Posts Page!</h1>
+
+            {data ? (
+                data.map((dt:any) => (
+                    <p key={dt.id}>{dt.id} <br /> {dt.title} <br /> {dt.body} <hr /></p> // Use dt.id instead of data.id
+                ))
+            ) : (
+                <p>Loading data...</p>
+            )}
+
+
+
         </>
     );
+}
 
+async function fetchData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        return await response.json()
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
 }
